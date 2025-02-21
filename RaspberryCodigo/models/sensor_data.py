dac25a303095e924f5e1add3cbf63283f7877398
ddsal07 <danidiaz0799@gmail.com>
2025-02-21 13:18:02 -0500

    Se crear servicio para poder ejecutar automaticamente el scipt al encender la rasp

--- RaspberryCodigo/models/sensor_data.py
+++ RaspberryCodigo/models/sensor_data.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # Conectar a la base de datos
 def get_db_connection():
-    conn = sqlite3.connect('sensor_data.db')
+    conn = sqlite3.connect('/home/stevpi/Desktop/proyectoGradoUD/sensor_data.db')  # Actualizar la ruta de la base de datos
     conn.row_factory = sqlite3.Row
     return conn
 
--- RaspberryCodigo/routes/actuator_routes.py
+++ RaspberryCodigo/routes/actuator_routes.py
@@ -1,20 +1,8 @@
 from flask import Blueprint, request, jsonify
-from models.actuator import save_actuator_state, get_db_connection
 from mqtt_client import publish_message
-from models.event import save_event  # Importar la función para guardar eventos
+from models.event import save_event  # Importar la funcion para guardar eventos
 
 actuator_bp = Blueprint('actuator_bp', __name__)
-
-# API para obtener estados de actuadores desde la base de datos
-@actuator_bp.route('/Actuator', methods=['GET'])
-def get_actuators():
-    page = int(request.args.get('page', 1))
-    page_size = int(request.args.get('pageSize', 10))
-    conn = get_db_connection()
-    data = conn.execute('SELECT * FROM actuators ORDER BY timestamp DESC LIMIT ? OFFSET ?',
-                        (page_size, (page - 1) * page_size)).fetchall()
-    conn.close()
-    return jsonify([dict(row) for row in data])
 
 # API para encender/apagar la luz del ESP32
 @actuator_bp.route('/Actuator/toggle_light', methods=['POST'])
