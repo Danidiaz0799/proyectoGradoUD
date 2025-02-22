3a8b25239ebe0ab0f95436ed5eb44fe9775addcb
ddsal07 <danidiaz0799@gmail.com>
2025-02-22 03:44:27 -0500

    se guarda version

--- Raspberry_Cliente/config/wifi_config.py
+++ Raspberry_Cliente/config/wifi_config.py
@@ -4,13 +4,8 @@ from config import config
 def connect_wifi():
     ssid = config.SSID
     password = config.PASSWORD
-    
+
     try:
-        # Escanear redes Wi-Fi disponibles
-        result = subprocess.run(['nmcli', 'dev', 'wifi'], capture_output=True, text=True, check=True)
-        print("Redes Wi-Fi disponibles:")
-        print(result.stdout)
-        
         # Conectar a la red Wi-Fi
         subprocess.run(['sudo', 'nmcli', 'd', 'wifi', 'connect', ssid, 'password', password], check=True)
         print("Conexion Wi-Fi establecida")
