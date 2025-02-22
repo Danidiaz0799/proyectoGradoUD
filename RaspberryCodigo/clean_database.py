b4861ced450e5aacd6661bf4d0b75a82a431f259
ddsal07 <danidiaz0799@gmail.com>
2025-02-21 19:52:31 -0500

    script para limpiar tablas y tiempo de espera para advertencias 60 seg

---
+++ RaspberryCodigo/clean_database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+def clean_tables():
+    conn = sqlite3.connect('sensor_data.db')
+    c = conn.cursor()
+
+    # Limpiar datos de la tabla dht_data
+    c.execute('DELETE FROM dht_data')
+    print("Datos de la tabla dht_data limpiados.")
+
+    # Limpiar datos de la tabla events
+    c.execute('DELETE FROM events')
+    print("Datos de la tabla events limpiados.")
+
+    # Limpiar datos de la tabla actuators
+    c.execute('DELETE FROM actuators')
+    print("Datos de la tabla actuators limpiados.")
+
+    conn.commit()
+    conn.close()
+    print("Base de datos limpiada.")
+
+# Ejecutar la funcion para limpiar las tablas si el archivo se ejecuta directamente
+if __name__ == '__main__':
+    clean_tables()
--- RaspberryCodigo/mqtt_client.py
+++ RaspberryCodigo/mqtt_client.py
@@ -1,11 +1,15 @@
 import paho.mqtt.client as mqtt
 from models.sensor_data import save_sensor_data
 from models.event import save_event  # Importar la funcion para guardar eventos
+import time
 
 client = None
+last_temp_event_time = 0
+last_hum_event_time = 0
 
 # Funcion de callback cuando se recibe un mensaje MQTT
 def on_message(client, userdata, msg):
+    global last_temp_event_time, last_hum_event_time
     try:
         # Decodificar el mensaje en UTF-8 y manejar errores
         data = msg.payload.decode('utf-8', errors='ignore').split(',')
@@ -13,11 +17,16 @@ def on_message(client, userdata, msg):
             temperatura, humedad = data[0], data[1]  # Separar temperatura y humedad
             print(f'Temperatura: {temperatura}C, Humedad: {humedad}')  # Imprimir datos en consola
             save_sensor_data(temperatura, humedad)  # Guardar datos en la base de datos
+            current_time = time.time()
             # Verificar si la temperatura o la humedad estan fuera de los parametros
             if not (20 <= float(temperatura) <= 30):  # Rango de temperatura aceptable
-                save_event(f"Advertencia: Temperatura fuera de rango - {temperatura}", "temperatura")
+                if current_time - last_temp_event_time > 60:
+                    save_event(f"Advertencia: Temperatura fuera de rango - {temperatura}", "temperatura")
+                    last_temp_event_time = current_time
             if not (60 <= float(humedad) <= 90):  # Rango de humedad aceptable
-                save_event(f"Advertencia: Humedad fuera de rango - {humedad}", "humedad")
+                if current_time - last_hum_event_time > 60:
+                    save_event(f"Advertencia: Humedad fuera de rango - {humedad}", "humedad")
+                    last_hum_event_time = current_time
         else:
             print("Datos recibidos en formato incorrecto")
     except Exception as e:
