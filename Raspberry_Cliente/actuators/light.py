f32a55ee0f46ec77fdaef6d345547f460344ad97
ddsal07 <danidiaz0799@gmail.com>
2025-02-26 19:36:32 -0500

    Se cambia Pin

--- Raspberry_Cliente/actuators/light.py
+++ Raspberry_Cliente/actuators/light.py
@@ -2,8 +2,9 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-light_pin = 2
+light_pin = 26
 GPIO.setup(light_pin, GPIO.OUT)
 
 def control_light(state):
     GPIO.output(light_pin, GPIO.HIGH if state == 'true' else GPIO.LOW)
+    print(f"Luz {'encendida' if state == 'true' else 'apagada'}")
