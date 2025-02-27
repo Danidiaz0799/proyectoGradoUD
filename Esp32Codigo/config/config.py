2ce8be28e82274a357c9f39589598a37e8db23fd
ddsal07 <danidiaz0799@gmail.com>
2025-02-26 22:11:25 -0500

    Se elimina ESP32 de proyecto

--- Esp32Codigo/config/config.py
+++
@@ -1,8 +0,0 @@
-# Configuracion de la red Wi-Fi1233456
-SSID = 'StevPoco'  # Nombre de la red Wi-Fi
-PASSWORD = 'hola1234567'  # Contrasena de la red Wi-Fi
-
-# Configuracion del cliente MQTT
-SERVER = '192.168.20.44'  # IP del servidor MQTT (Raspberry Pi)
-CLIENT_ID = 'ESP32_DHT11_Sensor'  # ID del cliente MQTT
-TOPIC = b'temperatura_humedad'  # Topico donde se publicaran los datos
